12f37a8b120dda2a0fad428eda1e96091a4626f7
phatnguyengithub <156379267+phatnguyengithub@users.noreply.github.com>
2024-01-12 06:46:44 +0700

    Add files via upload

---
+++ idk.lua
@@ -0,0 +1,3 @@
+_G.Name = "tphat hub" -- Put You Name Script Here
+_G.Logo = "15943864909" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
